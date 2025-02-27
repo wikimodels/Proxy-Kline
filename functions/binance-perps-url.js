434071a01927c38ba319a1e4e469a41bb5d4f88b
wikimodels <young.wiki.models@gmail.com>
2025-02-27 04:50:52 +0300

    test-shit 12

--- functions/binance-perps-url.js
+++ functions/binance-perps-url.js
@@ -1,5 +1,4 @@
-function binancePerpUrl(symbol, interval, limit) {
+export function binancePerpUrl(symbol, interval, limit) {
   const baseUrl = "https://fapi.binance.com";
   return `${baseUrl}/fapi/v1/klines?symbol=${symbol}&interval=${interval}&limit=${limit}`;
 }
-module.exports = { binancePerpUrl };
--- functions/bybit-perps-url.js
+++ functions/bybit-perps-url.js
@@ -1,5 +1,4 @@
-function bybitPerpUrl(symbol, interval, limit, category) {
+export function bybitPerpUrl(symbol, interval, limit, category) {
   const baseUrl = "https://api.bybit.com/v5/market/kline";
   return `${baseUrl}?category=${category}&symbol=${symbol}&interval=${interval}&limit=${limit}`;
 }
-module.exports = { bybitPerpUrl };
