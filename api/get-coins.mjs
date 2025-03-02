ebecae176cd7febaaa8f0efee9c7299ddfafae0c
wikimodels <young.wiki.models@gmail.com>
2025-03-02 19:46:42 +0300

    test-coins

---
+++ api/get-coins.mjs
@@ -0,0 +1,34 @@
+import { Redis } from "@upstash/redis";
+
+export const config = {
+  runtime: "edge",
+  regions: ["fra1"],
+};
+
+export default async function handler(request) {
+  const redis = new Redis({
+    url: process.env.KV_REST_API_URL,
+    token: process.env.KV_REST_API_TOKEN,
+  });
+
+  try {
+    const key = "coins";
+
+    // Retrieve the stored value
+    const storedData = await redis.get(key);
+
+    // If it's a string, parse it; otherwise, return as is
+    const parsedData =
+      typeof storedData === "string" ? JSON.parse(storedData) : storedData;
+
+    return new Response(JSON.stringify({ coins: parsedData }), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    return new Response(
+      JSON.stringify({ error: "Server error", details: error.message }),
+      { status: 500, headers: { "Content-Type": "application/json" } }
+    );
+  }
+}
---
+++ api/store-coins.mjs
@@ -0,0 +1,49 @@
+import { fetchCoins } from "../functions/fetch-coins.mjs";
+import { Redis } from "@upstash/redis";
+
+export const config = {
+  runtime: "edge",
+  regions: ["fra1"],
+};
+
+export default async function handler(request) {
+  const redis = new Redis({
+    url: process.env.KV_REST_API_URL,
+    token: process.env.KV_REST_API_TOKEN,
+  });
+
+  try {
+    const coins = await fetchCoins();
+
+    if (!Array.isArray(coins)) {
+      return new Response(
+        JSON.stringify({ error: "Invalid data format from MongoDB" }),
+        { status: 500, headers: { "Content-Type": "application/json" } }
+      );
+    }
+
+    const key = "coins";
+    await redis.set(key, JSON.stringify(coins));
+
+    // Retrieve the stored value
+    const storedData = await redis.get(key);
+
+    // Debugging: Log stored data type
+    console.log("Stored Data Type:", typeof storedData);
+    console.log("Stored Data:", storedData);
+
+    // If it's a string, parse it; otherwise, return as is
+    const parsedData =
+      typeof storedData === "string" ? JSON.parse(storedData) : storedData;
+
+    return new Response(JSON.stringify({ coins: parsedData }), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    return new Response(
+      JSON.stringify({ error: "Server error", details: error.message }),
+      { status: 500, headers: { "Content-Type": "application/json" } }
+    );
+  }
+}
